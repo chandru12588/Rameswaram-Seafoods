dd805711d96012538241a64b58c19bb2281a55dd
chandru12588 <chandru.jerry@gmail.com>
2025-12-28 01:29:46 +0530

    updated API endpoint

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // <-- aligned to backend
+  baseURL: "https://rms-backend-44od.onrender.com/api" // <-- correct backend URL
 });
 
 export default api;
